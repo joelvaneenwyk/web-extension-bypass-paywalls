31c206cb5e51265730a7d715e7cce9dceab4bf22
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2023-06-11 19:57:23 -0700

    Minor build script updates

--- build/build.sh
+++ build/build.sh
@@ -4,10 +4,10 @@ DOC_FILES="../README.md"
 IMG_FILES="../src/icons/bypass.png"
 
 # Strip subdirectories for Chrome manifest
-sed 's/src\/.*\///g' ../manifest.json > temp-chrome-manifest.json
+sed 's/src\/.*\///g' ../manifest.json >temp-chrome-manifest.json
 
 # Strip subdirectories for Firefox manifest
-sed 's/src\/.*\///g' ../manifest-ff.json > temp-ff-manifest.json
+sed 's/src\/.*\///g' ../manifest-ff.json >temp-ff-manifest.json
 
 # Fix update url for Chrome manifest
 sed -i 's/updates\.xml/src\/updates\/updates.xml/g' temp-chrome-manifest.json
@@ -16,13 +16,13 @@ sed -i 's/updates\.xml/src\/updates\/updates.xml/g' temp-chrome-manifest.json
 sed -i 's/updates\.json/src\/updates\/updates.json/g' temp-ff-manifest.json
 
 # Strip subdirectories for background.js
-sed 's/src\/.*\///g' ../src/js/background.js > temp-background.js
+sed 's/src\/.*\///g' ../src/js/background.js >temp-background.js
 
 # Strip subdirectories for options html file
-sed 's/\.\.\/js\///g' ../src/html/options.html > temp-options.html
+sed 's/\.\.\/js\///g' ../src/html/options.html >temp-options.html
 
 # Strip subdirectories for popup html file
-sed 's/\.\.\/js\///g' ../src/html/popup.html > temp-popup.html
+sed 's/\.\.\/js\///g' ../src/html/popup.html >temp-popup.html
 
 HTML_FILES="temp-options.html temp-popup.html"
 JS_FILES="../src/js/common.js ../src/js/sites.js ../src/js/contentScript.js ../src/js/options.js ../src/js/version.js temp-background.js"
@@ -35,6 +35,11 @@ NAME="output/bypass-paywalls"
 # Remove existing files
 rm -f $NAME.crx $NAME.xpi
 
+if ! 7z -h >/dev/null; then
+    sudo apt-get update
+    sudo apt-get install p7zip-full p7zip-rar
+fi
+
 # Generate Chrome .crx extension package
 7z a -tzip -mx9 $NAME.crx $CH_FILES
 7z rn $NAME.crx temp-chrome-manifest.json manifest.json temp-background.js background.js temp-options.html options.html temp-popup.html popup.html
@@ -44,5 +49,5 @@ rm -f $NAME.crx $NAME.xpi
 7z rn $NAME.xpi temp-ff-manifest.json manifest.json temp-background.js background.js temp-options.html options.html temp-popup.html popup.html
 
 # Remove temp files
-echo "\nDeleting temp files..."
+printf "\nDeleting temp files...\n"
 rm -v temp-chrome-manifest.json temp-ff-manifest.json temp-background.js temp-options.html temp-popup.html
