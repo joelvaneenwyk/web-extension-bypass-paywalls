b3a3e94524e8ffe9e9474e3b3692a4aa48b83536
joelvaneenwyk <joel.vaneenwyk@gmail.com>
2023-09-30 19:25:09 -0700

    chore: Minor config updates

--- eslint.config.js
+++ eslint.config.js
@@ -7,12 +7,22 @@
 import globals from "globals";
 
 import js from "@eslint/js";
+import pluginTypescript from '@typescript-eslint/eslint-plugin';
+import parserTypescript from '@typescript-eslint/parser';
+import pluginFunctional from 'eslint-plugin-functional';
+import pluginImport from 'eslint-plugin-import'; // 'import' is ambiguous & prettier has trouble
 
 /** @type { import("eslint").Linter.FlatConfig[] } */
 export default [
   js.configs.recommended,
   {
     languageOptions: {
+      parser: parserTypescript,
+      parserOptions: {
+        ecmaFeatures: { modules: true },
+        ecmaVersion: 'latest',
+        project: './tsconfig.json',
+      },
       globals: {
         ...globals.browser,
         ...globals.es2021,
@@ -20,6 +30,18 @@ export default [
         ...globals.commonjs
       }
     },
+    plugins: {
+      functional: pluginFunctional,
+      import: pluginImport,
+      '@typescript-eslint': pluginTypescript,
+      ts: pluginTypescript,
+    },
+    rules: {
+      ...pluginTypescript.configs['eslint-recommended'].rules,
+      ...pluginTypescript.configs['recommended'].rules,
+
+      'ts/return-await': 2,
+    }
     // rules: {
     //   "semi": [2, "always"],
     //   "@typescript-eslint/semi": [2, "always"]
