cbb01070c84b657c4d3258118362796e10171145
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-04-27 20:37:01 -0700

    Updated build script with more error handling and fix Windows support

--- build/build.sh
+++ build/build.sh
@@ -1,93 +1,129 @@
 #!/usr/bin/env bash
 
-set -eux
+set -eEau pipefail
 
-BRANCH_ROOT="$(cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")" &>/dev/null && cd ../ && pwd)"
+function _7z() {
+    if command -v 7z >/dev/null; then
+        7z "$@" | tr -s '\n'
+    else
+        _path=~/scoop/shims/7z.exe
+        if [ -f "$_path" ]; then
+            "$_path" "$@"
+        fi
+    fi
+}
 
-OUTPUT_DIR="${BRANCH_ROOT}/dist"
-TEMP_DIR="${BRANCH_ROOT}/.build"
-mkdir -p "${OUTPUT_DIR}" "${TEMP_DIR}"
+function rename_file() {
+    local archive_dir
+    archive_dir="$(dirname -- "${1:-}")"
 
-DOC_FILES="${BRANCH_ROOT}/README.md"
-IMG_FILES="${BRANCH_ROOT}/src/icons/bypass.png"
+    local archive_filename
+    archive_filename="$(basename -- "${1:-}")"
 
-# Strip subdirectories for Chrome manifest
-sed 's/src\/.*\///g' "${BRANCH_ROOT}/manifest.json" >"${TEMP_DIR}/temp-chrome-manifest.json"
+    local filename
+    filename="$(basename -- "${2:-}")"
 
-# Fix update url for Chrome manifest
-sed -i 's/updates\.xml/src\/updates\/updates.xml/g' "${TEMP_DIR}/temp-chrome-manifest.json"
+    local target="${filename/temp-/}"
+    target="${target/ff-/}"
+    if [[ "$filename" != "$target" ]]; then
+        cd "${archive_dir}" || return 2
+        _7z rn "$archive_filename" "${filename}" "${target}"
+    fi
 
-# Strip subdirectories for Firefox manifest
-sed 's/src\/.*\///g' "${BRANCH_ROOT}/manifest-ff.json" >"${TEMP_DIR}/temp-ff-manifest.json"
+    return 0
+}
 
-# Fix update url for Firefox manifest
-sed -i 's/updates\.json/src\/updates\/updates.json/g' "${TEMP_DIR}/temp-ff-manifest.json"
+function build() {
+    BRANCH_ROOT="$(cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")" &>/dev/null && cd ../ && pwd)"
 
-# Strip subdirectories for background.js
-sed 's/src\/.*\///g' "${BRANCH_ROOT}/src/js/background.js" >"${TEMP_DIR}/temp-background.js"
+    OUTPUT_DIR="${BRANCH_ROOT}/dist"
+    TEMP_DIR="${BRANCH_ROOT}/.build"
+    mkdir -p "${OUTPUT_DIR}" "${TEMP_DIR}"
 
-# Strip subdirectories for options html file
-sed 's/\.\.\/js\///g' "${BRANCH_ROOT}/src/html/options.html" >"${TEMP_DIR}/temp-options.html"
+    if ! command -v 7z >/dev/null; then
+        if command -v sudo >/dev/null && command -v apt-get >/dev/null; then
+            sudo apt-get update
+            sudo apt-get install p7zip-full p7zip-rar
+        fi
+    fi
 
-# Strip subdirectories for popup html file
-sed 's/\.\.\/js\///g' "${BRANCH_ROOT}/src/html/popup.html" >"${TEMP_DIR}/temp-popup.html"
+    if ! _7z -h >/dev/null 2>&1; then
+        echo "7z is not installed. Please install it manually."
+        return 1
+    fi
 
-HTML_FILES=(
-    "${TEMP_DIR}/temp-options.html"
-    "${TEMP_DIR}/temp-popup.html")
-JS_FILES=(
-    "${BRANCH_ROOT}/src/js/common.js"
-    "${BRANCH_ROOT}/src/js/sites.js"
-    "${BRANCH_ROOT}/src/js/contentScript.js"
-    "${BRANCH_ROOT}/src/js/options.js"
-    "${BRANCH_ROOT}/src/js/version.js"
-    "${TEMP_DIR}/temp-background.js")
-GEN_FILES=(
-    "${JS_FILES[@]}"
-    "${HTML_FILES[@]}"
-    "${DOC_FILES[@]}"
-    "${IMG_FILES[@]}")
-CH_FILES=(
-    "${GEN_FILES[@]}"
-    "${TEMP_DIR}/temp-chrome-manifest.json"
-    "${BRANCH_ROOT}/src/updates/updates.xml")
-FF_FILES=(
-    "${GEN_FILES[@]}"
-    "${TEMP_DIR}/temp-ff-manifest.json"
-    "${BRANCH_ROOT}/src/updates/updates.json"
-    "${BRANCH_ROOT}/src/icons/bypass-dark.png")
+    # Strip subdirectories for Chrome manifest
+    sed 's/src\/.*\///g' "${BRANCH_ROOT}/manifest.json" >"${TEMP_DIR}/temp-chrome-manifest.json"
 
-if ! 7z -h >/dev/null; then
-    sudo apt-get update
-    sudo apt-get install p7zip-full p7zip-rar
-fi
+    # Fix update url for Chrome manifest
+    sed -i 's/updates\.xml/src\/updates\/updates.xml/g' "${TEMP_DIR}/temp-chrome-manifest.json"
 
-NAME="${OUTPUT_DIR}/bypass-paywalls"
+    # Strip subdirectories for Firefox manifest
+    sed 's/src\/.*\///g' "${BRANCH_ROOT}/manifest-ff.json" >"${TEMP_DIR}/temp-ff-manifest.json"
 
-# Remove existing output files
-rm -f "$NAME.crx" "$NAME.xpi"
+    # Fix update url for Firefox manifest
+    sed -i 's/updates\.json/src\/updates\/updates.json/g' "${TEMP_DIR}/temp-ff-manifest.json"
 
-# Generate Chrome .crx extension package
-7z a -tzip -mx9 "$NAME.crx" "${CH_FILES[@]}" | tr -s '\n'
-7z rn "$NAME.crx" \
-    "${TEMP_DIR}/temp-chrome-manifest.json" "${BRANCH_ROOT}/manifest.json" \
-    "${TEMP_DIR}/temp-background.js" "${BRANCH_ROOT}/background.js" \
-    "${TEMP_DIR}/temp-options.html" "${BRANCH_ROOT}/options.html" \
-    "${TEMP_DIR}/temp-popup.html" "${BRANCH_ROOT}/popup.html" | tr -s '\n'
+    # Strip subdirectories for background.js
+    sed 's/src\/.*\///g' "${BRANCH_ROOT}/src/js/background.js" >"${TEMP_DIR}/temp-background.js"
 
-# Generate Firefox .xpi extension package (firefox manifest)
-7z a -tzip -mx9 "$NAME.xpi" "${FF_FILES[@]}" | tr -s '\n'
-7z rn "$NAME.xpi" \
-    "${TEMP_DIR}/temp-ff-manifest.json" "${BRANCH_ROOT}/manifest.json" \
-    "${TEMP_DIR}/temp-background.js" "${BRANCH_ROOT}/background.js" \
-    "${TEMP_DIR}/temp-options.html" "${BRANCH_ROOT}/options.html" \
-    "${TEMP_DIR}/temp-popup.html" "${BRANCH_ROOT}/popup.html" | tr -s '\n'
+    # Strip subdirectories for options html file
+    sed 's/\.\.\/js\///g' "${BRANCH_ROOT}/src/html/options.html" >"${TEMP_DIR}/temp-options.html"
 
-# Remove temp files
-rm -v \
-    "${TEMP_DIR}/temp-chrome-manifest.json" \
-    "${TEMP_DIR}/temp-ff-manifest.json" \
-    "${TEMP_DIR}/temp-background.js" \
-    "${TEMP_DIR}/temp-options.html" \
-    "${TEMP_DIR}/temp-popup.html"
-printf "Removed temporary files...\n"
+    # Strip subdirectories for popup html file
+    sed 's/\.\.\/js\///g' "${BRANCH_ROOT}/src/html/popup.html" >"${TEMP_DIR}/temp-popup.html"
+
+    DOC_FILES="${BRANCH_ROOT}/README.md"
+    IMG_FILES="${BRANCH_ROOT}/src/icons/bypass.png"
+    HTML_FILES=(
+        "${TEMP_DIR}/temp-options.html"
+        "${TEMP_DIR}/temp-popup.html")
+    JS_FILES=(
+        "${BRANCH_ROOT}/src/js/common.js"
+        "${BRANCH_ROOT}/src/js/sites.js"
+        "${BRANCH_ROOT}/src/js/contentScript.js"
+        "${BRANCH_ROOT}/src/js/options.js"
+        "${BRANCH_ROOT}/src/js/version.js"
+        "${TEMP_DIR}/temp-background.js")
+    GEN_FILES=(
+        "${JS_FILES[@]}"
+        "${HTML_FILES[@]}"
+        "${DOC_FILES[@]}"
+        "${IMG_FILES[@]}")
+    CH_FILES=(
+        "${GEN_FILES[@]}"
+        "${TEMP_DIR}/temp-chrome-manifest.json"
+        "${BRANCH_ROOT}/src/updates/updates.xml")
+    FF_FILES=(
+        "${GEN_FILES[@]}"
+        "${TEMP_DIR}/temp-ff-manifest.json"
+        "${BRANCH_ROOT}/src/updates/updates.json"
+        "${BRANCH_ROOT}/src/icons/bypass-dark.png")
+
+    NAME="${OUTPUT_DIR}/bypass-paywalls"
+
+    # Generate Chrome .crx extension package
+    rm -f "$NAME.crx"
+    _7z a -tzip -bb1 -mx9 "$NAME.crx" "${CH_FILES[@]}"
+    for value in "${CH_FILES[@]}"; do
+        rename_file "$NAME.crx" "$value"
+    done
+
+    # Generate Firefox .xpi extension package
+    rm -f "$NAME.xpi"
+    _7z a -tzip -bb1 -mx9 "$NAME.xpi" "${FF_FILES[@]}"
+    for value in "${FF_FILES[@]}"; do
+        rename_file "$NAME.xpi" "$value"
+    done
+
+    # Remove temp files
+    files=("${CH_FILES[@]}" "${FF_FILES[@]}")
+    for value in "${files[@]}"; do
+        if [[ $value == *"${TEMP_DIR}"* ]]; then
+            rm -fv "$value"
+        fi
+    done
+    printf "Removed temporary files...\n"
+}
+
+build "$@"
